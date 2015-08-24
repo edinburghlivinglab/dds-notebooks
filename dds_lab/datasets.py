f75018f0ae2c02d031ee569e233e891d4d0e5f89
Ewan Klein <ewan.klein@gmail.com>
2015-08-24 17:38:46 +0100

    Fixed path bug

--- dds_lab/datasets.py
+++ dds_lab/datasets.py
@@ -1,6 +1,6 @@
 import os
 
-data_dir = "../data"
+data_dir = os.path.abspath("../data")
 
 eps_extract = os.path.join(data_dir, "Edinburgh_People_Survey/EPS_2013_Data_extract.csv")
-schools = os.path.join(data_dir, "/opendatascotland/tutorial_1/schools.csv")
+schools = os.path.join(data_dir, "opendatascotland/tutorial_1/schools.csv")
