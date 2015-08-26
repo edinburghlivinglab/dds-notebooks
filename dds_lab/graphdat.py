1b9a95f21d43dc241546db6bc1d39e32178f6ff6
franciscovargas <vargfran@gmail.com>
2015-08-26 15:31:48 +0100

    Finished small section on adjecency lists, will add small iteration exercise at the end

--- dds_lab/graphdat.py
+++ dds_lab/graphdat.py
@@ -2,61 +2,6 @@ from bokeh.plotting import show, figure
 from bokeh.models import HoverTool, ColumnDataSource, Circle, Text
 from collections import OrderedDict
 import numpy as np
-from queue import Queue
-
-
-def test_binary_tree(adjencency_list):
-    """
-    Hardcoded bokeh 3 level completely balanced binary Tree
-    Attempting to automate to n level trees in the function
-    bellow. Hardcoded example need to automate.
-    """
-    fig = figure(
-        title="DDS Help Tree",
-        tools="box_zoom,box_select,resize,reset"  # Note no hover
-    )
-    names = ["Gavin Gray", "Ewan Klein",
-             "Francisco Vargas", "S3", "S1", "S2", "S4"]
-    info = ["TA", "Lectuer", "TA", "Student", "Student", "Student", "Student"]
-    fig.xgrid.grid_line_color = None
-    fig.ygrid.grid_line_color = None
-    fig.axis.major_label_text_font_size = "0pt"
-    fig.axis.major_tick_line_color = None
-    fig.axis[0].ticker.num_minor_ticks = 0
-    fig.axis[1].ticker.num_minor_ticks = 0
-    fig.outline_line_color = "white"
-    fig.xaxis.axis_line_color = "white"
-    fig.yaxis.axis_line_color = "white"
-    fig.line(x=[1, 3, 5], y=[1, 2, 1])
-    fig.line(x=[0, 1, 2], y=[0, 1, 0])
-    fig.line(x=[4, 5, 6], y=[0, 1, 0])
-
-    source = ColumnDataSource(
-        data=dict(
-            xname=[1, 3, 5, 6, 0, 2, 4],
-            yname=[1, 2, 1, 0, 0, 0, 0],
-            name=names,
-            info=info
-        )
-    )
-    fig.annulus(x='xname', y='yname', inner_radius=0.29,
-                outer_radius=0.295, source=source)
-
-    circle = Circle(x="xname", y="yname", radius=0.29, fill_color="#e9f1f8")
-    circle_renderer = fig.add_glyph(source, circle)
-
-    x1, y1 = (-0.1, -0.04)
-    fig.text(x=[-0.1, x1 + 2, x1 + 4, x1+6],
-             y=[y1, y1, y1, y1], text=["S1", "S2", "S3", "S4"])
-    fig.text(x=[x1 + 0.9, x1 + 5], y=[y1 + 1, y1 + 1], text=["GG", "FV"])
-    fig.text(x=[x1 + 3], y=[y1 + 2], text=["EK"])
-
-    tooltips = OrderedDict([
-        ("name", "@name"),
-        ("info", "@info"),
-    ])
-    fig.add_tools(HoverTool(tooltips=tooltips, renderers=[circle_renderer]))
-    return fig, adjencency_list
 
 
 def find_height(node_key, adjencency_list):
@@ -107,20 +52,35 @@ def level_dict(adj_list, curr_elems, order=0):
     return d
 
 
-def get_tree_plot(adjencency_list, names, info):
+def get_tree_plot(adjencency_list, names, info, title):
     """
     BFS-traversal (level order) and plot of tree.
     """
+    if not adjencency_list:
+        print("Please provide an adjencency_list along with data")
+        fig_null = figure(title=title)
+        # To suppress no glyph bug message:
+        fig_null.line(x=[0,1],y=[0,1], line_color="white")
+
+        # Keep the same format TODO: abstract
+        fig_null.xgrid.grid_line_color = None
+        fig_null.ygrid.grid_line_color = None
+        fig_null.axis.major_label_text_font_size = "0pt"
+        fig_null.axis.major_tick_line_color = None
+        fig_null.axis[0].ticker.num_minor_ticks = 0
+        fig_null.axis[1].ticker.num_minor_ticks = 0
+        fig_null.outline_line_color = "white"
+        fig_null.xaxis.axis_line_color = "white"
+        fig_null.yaxis.axis_line_color = "white"
+        return fig_null, adjencency_list
     x = []
     y = []
 
-    q = Queue()
     root = find_root(adjencency_list)
     # print adjencency_list
     # This is level numebers counting from one
     # so not actually height
     height = find_height(root, adjencency_list) + 1
-    q.put(root)
     o = level_dict(adjencency_list, [(root, None)])
 
     x = []
@@ -135,11 +95,16 @@ def get_tree_plot(adjencency_list, names, info):
     dist_dict = {}
     linesx = []
     linesy = []
-    fig = figure()
+
+    fig = figure(
+        title=title,
+        tools="box_select,resize,reset"  # Note no hover
+    )
+
     textsx = []
     textsy = []
     texts = []
-    print(type(root))
+
     textsx.append(0 - 0.18)
     textsy.append(0-0.07)
     texts.append(root)
@@ -206,3 +171,30 @@ def get_tree_plot(adjencency_list, names, info):
     fig.text(x=textsx,y=textsy,text=texts)
 
     return fig, adjencency_list
+
+
+def adjecency_list_exercise():
+    names = ["Sentence",
+             "Noun phrase",
+             "Verb phrase",
+             "Verb",
+             "Noun phrase",
+             "Delimiter",
+             "Noun"]
+    info = ["John hit the ball",
+            "John",
+            "hit the ball",
+            "hit",
+            "the ball",
+            "the",
+            "ball"]
+    # the adjecency list is more or less exactly the same as our table without the information collumn:
+    adjecency_list = {"S": ["NP", "VP"],
+                      "VP": ["V", "NP`"],
+                      "NP`": ["D", "N"]}
+    #Generate tree plot
+    fig, ad_list = get_tree_plot(adjecency_list,
+                                names=names,
+                                info=info,
+                                title="Parse Tree")
+    return fig
